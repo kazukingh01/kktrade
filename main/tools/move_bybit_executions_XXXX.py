068b18ed4439d8f7b514cc6c248c6ca84fcc0683
kazukingh01 <kazukingh01@gmail.com>
2023-10-25 15:58:27 +0900

    modify

--- main/tools/move_bybit_executions_XXXX.py
+++ main/tools/move_bybit_executions_XXXX.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, datetime
 import numpy as np
 from tqdm import tqdm
 # local package
@@ -10,14 +10,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--fr", type=str, help="db from")
     parser.add_argument("--to", type=str, help="db to")
+    parser.add_argument("--since", type=datetime.datetime.fromisoformat)
+    parser.add_argument("--until", type=datetime.datetime.fromisoformat)
     parser.add_argument("--tbl", type=str, help="table name")
     args = parser.parse_args()
     print(args)
     DB_from = Psgre(f"host={args.fr} port={PORT} dbname={DBNAME} user={USER} password={PASS}", max_disp_len=200)
     DB_to   = Psgre(f"host={args.to} port={PORT} dbname={DBNAME} user={USER} password={PASS}", max_disp_len=200)
-    dfbase  = DB_from.select_sql(f"select id from {args.tbl};")
-    dfwk    = DB_to  .select_sql(f"select id from {args.tbl};")
-    dfbase  = dfbase.loc[~dfbase["id"].isin(dfwk["id"])]
+    if args.since is not None and args.until is not None:
+        assert args.since < args.until
+        dfbase = DB_from.select_sql(f"select id from {args.tbl} where unixtime >= {int(args.since.timestamp()) * 1000} and unixtime <= {int(args.until.timestamp()) * 1000};")
+        dfwk   = DB_to  .select_sql(f"select id from {args.tbl} where unixtime >= {int(args.since.timestamp()) * 1000} and unixtime <= {int(args.until.timestamp()) * 1000};")
+    else:
+        dfbase = DB_from.select_sql(f"select id from {args.tbl};")
+        dfwk   = DB_to  .select_sql(f"select id from {args.tbl};")
+    dfbase = dfbase.loc[~dfbase["id"].isin(dfwk["id"])]
     for index in tqdm(np.array_split(np.arange(dfbase.shape[0]), dfbase.shape[0] // 10000)):
         dfwk = dfbase.iloc[index].copy()
         sql  = f"select * from {args.tbl} where id in ('" + "','".join(dfwk["id"].tolist()) + "');"
