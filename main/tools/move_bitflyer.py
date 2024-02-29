2c4a938901174a4c8750a085e4684f4ed1f7592e
kazukingh01 <kazukingh01@gmail.com>
2024-02-29 19:05:03 +0900

    modify

--- main/tools/move_bitflyer.py
+++ main/tools/move_bitflyer.py
@@ -6,6 +6,12 @@ from tqdm import tqdm
 from kkpsgre.psgre import Psgre
 from kktrade.config.psgre import HOST, PORT, USER, PASS, DBNAME
 
+PKEY = {
+    "bitflyer_executions": ["symbol", "id"],
+    "bitflyer_orderbook": ["unixtime"],
+    "bitflyer_ticker": ["symbol", "tick_id"]
+}
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -17,34 +23,35 @@ if __name__ == "__main__":
     parser.add_argument("--until", type=datetime.datetime.fromisoformat)
     parser.add_argument("--tbl", type=str, help="table name")
     parser.add_argument("--num", type=int, default=10000)
-    parser.add_argument("--pkey", type=lambda x: x.split(","), default="id")
     parser.add_argument("--update", action='store_true', default=False)
     args = parser.parse_args()
     print(args)
+    assert args.tbl is not None
+    assert args.tbl in PKEY
     DB_from = Psgre(f"host={args.ipfr} port={args.portfr} dbname={DBNAME} user={USER} password={PASS}", max_disp_len=200)
     DB_to   = Psgre(f"host={args.ipto} port={args.portto} dbname={DBNAME} user={USER} password={PASS}", max_disp_len=200)
     if args.since is not None and args.until is not None:
         assert args.since < args.until
-        dfbase = DB_from.select_sql(f"select {','.join(args.pkey)} from {args.tbl} where unixtime >= {int(args.since.timestamp())} and unixtime <= {int(args.until.timestamp())};")
-        dfwk   = DB_to  .select_sql(f"select {','.join(args.pkey)} from {args.tbl} where unixtime >= {int(args.since.timestamp())} and unixtime <= {int(args.until.timestamp())};")
+        dfbase = DB_from.select_sql(f"select {','.join(PKEY[args.tbl])} from {args.tbl} where unixtime >= {int(args.since.timestamp())} and unixtime < {int(args.until.timestamp())};")
+        dfwk   = DB_to  .select_sql(f"select {','.join(PKEY[args.tbl])} from {args.tbl} where unixtime >= {int(args.since.timestamp())} and unixtime < {int(args.until.timestamp())};")
     else:
-        dfbase = DB_from.select_sql(f"select {','.join(args.pkey)} from {args.tbl};")
-        dfwk   = DB_to  .select_sql(f"select {','.join(args.pkey)} from {args.tbl};")
-    if len(args.pkey) == 1:
-        dfbase = dfbase.loc[~dfbase[args.pkey[0]].isin(dfwk[args.pkey[0]].unique())]
-        dfbase = dfbase.groupby(args.pkey[0]).first().reset_index(drop=False)
+        dfbase = DB_from.select_sql(f"select {','.join(PKEY[args.tbl])} from {args.tbl};")
+        dfwk   = DB_to  .select_sql(f"select {','.join(PKEY[args.tbl])} from {args.tbl};")
+    if len(PKEY[args.tbl]) == 1:
+        dfbase = dfbase.loc[~dfbase[PKEY[args.tbl][0]].isin(dfwk[PKEY[args.tbl][0]].unique())]
+        dfbase = dfbase.groupby(PKEY[args.tbl][0]).first().reset_index(drop=False)
     else:
         dfbase["__work"] = ""
         dfwk[  "__work"] = ""
-        for x in args.pkey:
+        for x in PKEY[args.tbl]:
             dfbase["__work"] = dfbase["__work"] + dfbase[x].astype(str) + ","
             dfwk[  "__work"] = dfwk[  "__work"] + dfwk[  x].astype(str) + ","
         dfbase = dfbase.loc[~dfbase["__work"].isin(dfwk["__work"].unique())]
         dfbase = dfbase.groupby("__work").first().reset_index(drop=True)
     for index in tqdm(np.array_split(np.arange(dfbase.shape[0]), dfbase.shape[0] // args.num)):
         dfwk = dfbase.iloc[index].copy()
-        if len(args.pkey) == 1:
-            sql  = f"select * from {args.tbl} where {args.pkey[0]} in (" + ",".join(dfwk[args.pkey[0]].astype(str).tolist()) + ");"
+        if len(PKEY[args.tbl]) == 1:
+            sql  = f"select * from {args.tbl} where {PKEY[args.tbl][0]} in (" + ",".join(dfwk[PKEY[args.tbl][0]].astype(str).tolist()) + ");"
         else:
             sql  = f"select * from {args.tbl} where "
             for ndf in dfwk.values:
