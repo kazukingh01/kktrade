0f3096a4802c84521c4ce0563065e631195fbd0f
kazukingh01 <kazukingh01@gmail.com>
2024-03-02 18:35:00 +0900

    modify

--- main/database/bybit/download_pastdata.py
+++ main/database/bybit/download_pastdata.py
@@ -40,8 +40,10 @@ def download_trade(symbol: str, date: datetime.datetime, tmp_file_path: str="./t
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--fr", type=datetime.datetime.fromisoformat, help="--fr 20200101", required=True)
-    parser.add_argument("--to", type=datetime.datetime.fromisoformat, help="--to 20200101", required=True)
+    parser.add_argument("--fr", type=lambda x: datetime.datetime.fromisoformat(str(x) + "T00:00:00Z"), help="--fr 20200101", required=True)
+    parser.add_argument("--to", type=lambda x: datetime.datetime.fromisoformat(str(x) + "T00:00:00Z"), help="--to 20200101", required=True)
+    parser.add_argument("--num", type=int, default=100)
+    parser.add_argument("--jobs", type=int, default=1)
     parser.add_argument("--update", action='store_true', default=False)
     kwargs_psgre = {
         "keepalives": 1,
@@ -49,27 +51,24 @@ if __name__ == "__main__":
         "keepalives_interval": 5,
         "keepalives_count": 100,
     }
-    args      = parser.parse_args()
-    DB        = Psgre(f"host={HOST} port={PORT} dbname={DBNAME} user={USER} password={PASS}", kwargs_psgre=kwargs_psgre, max_disp_len=200)
-    df_mst    = DB.select_sql(f"select * from master_symbol where is_active = true and exchange = '{EXCHANGE}'")
-    mst_id    = {y:x for x, y in df_mst[["symbol_id", "symbol_name"]].values}
+    args   = parser.parse_args()
+    DB     = Psgre(f"host={HOST} port={PORT} dbname={DBNAME} user={USER} password={PASS}", kwargs_psgre=kwargs_psgre, max_disp_len=200)
+    df_mst = DB.select_sql(f"select * from master_symbol where is_active = true and exchange = '{EXCHANGE}'")
+    mst_id = {y:x for x, y in df_mst[["symbol_id", "symbol_name"]].values}
     date_st, date_ed = args.fr, args.to
     assert date_st <= date_ed
     for date in [date_st + datetime.timedelta(days=x) for x in range((date_ed - date_st).days + 1)]:
         for symbol in mst_id.keys():
-            print(date, symbol)
+            DB.logger.info(f"date: {date}, symbol: {symbol}")
             if MST_CONV.get(symbol) is None: continue
             df = download_trade(symbol, date, mst_id=mst_id)
             if df.shape[0] > 0:
                 df_exist = DB.select_sql(f"select id from {EXCHANGE}_executions where symbol = {df['symbol'].iloc[0]} and unixtime >= {int(df['unixtime'].min())} and unixtime <= {int(df['unixtime'].max())};")
                 df = df.loc[~df["id"].isin(df_exist["id"])]
             else:
-                print("Nothing data.")
+                DB.logger.warning("Nothing data.")
                 continue
             if df.shape[0] > 0 and args.update:
-                if df.shape[0] > 10000:
-                    for indexes in tqdm(np.array_split(np.arange(df.shape[0]), df.shape[0] // 10000)):
-                        DB.insert_from_df(df.iloc[indexes], f"{EXCHANGE}_executions", is_select=True, n_jobs=8)
-                        DB.execute_sql()
-                else:
-                    DB.execute_copy_from_df(df, f"{EXCHANGE}_executions", filename=f"postgres.{symbol}.{date.strftime('%Y%m%d')}.csv", str_null="", n_jobs=8)
+                for indexes in tqdm(np.array_split(np.arange(df.shape[0]), df.shape[0] // args.num)):
+                    DB.insert_from_df(df.iloc[indexes], f"{EXCHANGE}_executions", is_select=True, n_jobs=args.jobs)
+                    DB.execute_sql()
