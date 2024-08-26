c374808f3025b2af797e4c3d59ef595ddf30d01f
kazukingh01 <kazukingh01@gmail.com>
2024-08-26 13:57:17 +0900

    modify

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'kkpsgre @ git+https://github.com/kazukingh01/kkpsgre.git@30d7022749b70cdae939707aad9448430fdd70f8',
+        'kkpsgre @ git+https://github.com/kazukingh01/kkpsgre.git@54940ffa7154ee4da98dcf874937d39f87abfbdb',
         'pandas==2.2.1',
         'numpy==1.26.4',
         'joblib==1.3.2',
