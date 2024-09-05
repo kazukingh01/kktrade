b8be64fef72a3308915b1907a5dfa3f25085e8d2
kazukingh01 <kazukingh01@gmail.com>
2024-09-06 00:37:27 +0900

    modify

--- main/mart/ohlc.py
+++ main/mart/ohlc.py
@@ -9,7 +9,7 @@ from kkpsgre.util.logger import set_logger
 
 EXCHANGES = ["bybit", "binance", "bitflyer"]
 LOGGER    = set_logger(__name__)
-INTERVALS = [60, 120]
+INTERVALS = [60, ]
 
 
 def convdatetime(x: str):
@@ -32,21 +32,27 @@ if __name__ == "__main__":
     df_mst = DB.select_sql(f"select * from master_symbol where is_active = true;")
     for exchange in EXCHANGES:
         LOGGER.info(f"exchange: {exchange}, from: {args.fr}, to: {args.to}")
-        df = DB.select_sql(f"select * from {exchange}_executions where side = 0 and unixtime >= {int(args.fr.timestamp())} and unixtime < {int(args.to.timestamp())};")
-        df = df.sort_values(["symbol", "unixtime"]).reset_index(drop=True)
-        df["amount"] = df["price"] * df["size"]
+        df = DB.select_sql(f"select * from {exchange}_executions where side in (0,1) and unixtime >= {int(args.fr.timestamp())} and unixtime < {int(args.to.timestamp())};")
+        df = df.sort_values(["symbol", "unixtime", "price"]).reset_index(drop=True)
+        df["amount"] = (df["price"] * df["size"])
         for interval in INTERVALS:
             df["timegroup"] = (df["unixtime"] // interval * interval)
-            ndf_tg  = np.arange(int(args.fr.timestamp()) // interval * interval, int(args.to.timestamp()) // interval * interval, interval, dtype=int)
-            dfwk    = df.groupby(["symbol", "timegroup"])["price"].aggregate(["max", "min", "first", "last"])
-            dfwk["size"]   = df.groupby(["symbol", "timegroup"])["size"  ].sum()
-            dfwk["amount"] = df.groupby(["symbol", "timegroup"])["amount"].sum()
-            dfwk["ntx"]    = df.groupby(["symbol", "timegroup"]).size()
-            dfwk["median"] = dfwk["amount"] / dfwk["size"]
-            ndfwk  = np.arange(0, 1.01, 0.1)
-            dfwkwk = df.groupby(["symbol", "timegroup"])["amount"].quantile(ndfwk)
-            for x in ndfwk[:-1]: dfwk[f"quantile_{str(int(x * 100)).zfill(2)}"] = dfwkwk.loc[slice(None), slice(None), x] / dfwk["amount"]
+            df["cumsum"]    = df.groupby(["symbol", "timegroup", "side"])["amount"].cumsum()
+            ndf_tg          = np.arange(int(args.fr.timestamp()) // interval * interval, int(args.to.timestamp()) // interval * interval, interval, dtype=int)
+            dfwk            = df.groupby(["symbol", "timegroup", "side"])["price"].aggregate(["max", "min", "first", "last"])
+            dfwkwk          = df.groupby(["symbol", "timegroup", "side"])[["size", "amount"]].sum()
+            dfwk["size"]    = dfwkwk.loc[:, "size"]
+            dfwk["amount"]  = dfwkwk.loc[:, "amount"]
+            dfwk["ntx"]     = df.groupby(["symbol", "timegroup", "side"]).size()
+            dfwk["ave"]     = dfwk["amount"] / dfwk["size"]
+            ndfwk           = np.arange(0.1, 1.0, 0.2)
+            dfwkwk          = df.groupby(["symbol", "timegroup", "side"])[["amount", "cumsum"]].quantile(ndfwk)
+            for x in ndfwk:
+                dfwk[f"amount_q{str(int(x * 100)).zfill(2)}"] = dfwkwk.loc[(slice(None), slice(None), slice(None), x)]["amount"] / dfwk["amount"]
+                dfwk[f"cumsum_q{str(int(x * 100)).zfill(2)}"] = dfwkwk.loc[(slice(None), slice(None), slice(None), x)]["cumsum"] / dfwk["amount"]
+            raise
             dfwk    = dfwk.reset_index()
             ndf_sbl = df["symbol"].unique()
-            df_base = pd.DataFrame(np.concatenate([np.repeat(ndf_sbl, ndf_tg.shape[0]).reshape(-1, 1), np.tile(ndf_tg, ndf_sbl.shape[0]).reshape(-1, 1)], axis=-1), columns=["symbol", "timegroup"])
-            df_base = pd.merge(df_base, dfwk, how="left", on=["symbol", "timegroup"])
+            df_ohlc = pd.DataFrame(np.concatenate([np.repeat(ndf_sbl, ndf_tg.shape[0]).reshape(-1, 1), np.tile(ndf_tg, ndf_sbl.shape[0]).reshape(-1, 1)], axis=-1), columns=["symbol", "timegroup"])
+            df_ohlc = pd.merge(df_ohlc, dfwk, how="left", on=["symbol", "timegroup"])
+            raise
