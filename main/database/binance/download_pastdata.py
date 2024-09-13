15c5080003b92eb3a96d2e5d36e0d8c96e50381f
kazukingh01 <kazukingh01@gmail.com>
2024-09-13 21:43:06 +0900

    modify binance past data download

--- main/database/binance/download_pastdata.py
+++ main/database/binance/download_pastdata.py
@@ -1,104 +1,113 @@
-import datetime, requests, gzip, argparse
+import datetime, requests, argparse, json
+from zipfile import ZipFile
 from tqdm import tqdm
 from io import StringIO
 import pandas as pd
 import numpy as np
 # local package
-from kkpsgre.psgre import Psgre
-from kktrade.config.psgre import HOST, PORT, USER, PASS, DBNAME
-from getdata import EXCHANGE
+from kkpsgre.util.logger import set_logger
+from getdata import EXCHANGE, LS_RATIO_TYPE
 
 
-BASEURL  = "https://public.bybit.com/trading/"
+BASEURL  = "https://data.binance.vision/data/"
+BASEURL2 = "https://quote-saver.bycsi.com/orderbook/"
 MST_CONV = {
-    "linear@BTCUSDT": "BTCUSDT",
-    "inverse@BTCUSD": "BTCUSD",
-    "linear@ETHUSDT": "ETHUSDT",
-    "inverse@ETHUSD": "ETHUSD",
-    "linear@XRPUSDT": "XRPUSDT",
-    "inverse@XRPUSD": "XRPUSD",
+    "SPOT": "spot/",
+    "USDS": "futures/um/",
+    "COIN": "futures/cm/",
 }
+LOGGER = set_logger(__name__)
 
 
-import re
-from playwright.sync_api import Playwright, sync_playwright, expect
-
-
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://www.binance.com/en/landing/data")
-    page.get_by_role("button", name="Spot").click()
-    page.locator(".css-14vl80k > div:nth-child(2) > .landing-ui-simple-dropdown > .css-1cda2ax").click()
-    page.get_by_text("Monthly", exact=True).click()
-    page.get_by_label("Data Range").click()
-    page.get_by_label("Data Range").fill("2019-01")
-    page.get_by_label("Data Range").press("Tab")
-    page.get_by_role("textbox", name="YYYY-MM").click()
-    page.get_by_role("textbox", name="YYYY-MM").fill("2019-01")
-    page.locator("label").filter(has_text="Data Range").click()
-    page.get_by_role("textbox", name="YYYY-MM").press("Enter")
-    page.locator("div:nth-child(2) > .css-1cda2ax").first.click()
-    page.get_by_role("textbox", name="YYYY-MM").fill("TC")
-    page.get_by_placeholder("Search for Symbol (Max: 5)").click()
-    page.locator("div:nth-child(2) > .css-1cda2ax").first.click()
-    page.locator("form").get_by_role("img").first.click()
-    page.locator(".css-1lrh3c9 > path").click()
-    page.locator("div:nth-child(2) > .css-1cda2ax > .bn-input-suffix > .css-13c2b5p > .css-rrv9q1 > path").first.click()
-    page.get_by_placeholder("Search for Symbol (Max: 5)").click()
-    page.get_by_placeholder("Search for Symbol (Max: 5)").fill("BTCUSDT")
-    page.get_by_role("option", name="BTCUSDT", exact=True).locator("svg").click()
-    page.locator("div").filter(has_text=re.compile(r"^BTCUSDTBTCUSDTWBTCUSDT$")).get_by_role("img").nth(3).click()
-    page.get_by_placeholder("Search for Symbol (Max: 5)").click()
-    page.get_by_placeholder("Search for Symbol (Max: 5)").fill("BTCUSDC")
-    page.get_by_role("option", name="BTCUSDC").locator("svg").click()
-    page.locator("form div").filter(has_text="Field InformationCollapseField NameDescriptionidtrade").nth(1).click()
-    page.get_by_role("button", name="Reject Additional Cookies").click()
-    page.get_by_role("button", name="Confirm").click()
-    with page.expect_download() as download_info:
-        with page.expect_popup() as page1_info:
-            page.get_by_role("button", name="Download").click()
-        page1 = page1_info.value
-    download = download_info.value
-    page1.close()
-    page2.close()
-    page.get_by_role("button", name="Spot").click()
-    page.locator("div:nth-child(2) > .css-rk221x > path").click()
-    page.locator("div").filter(has_text=re.compile(r"^Confirm$")).nth(1).click()
-
-    # ---------------------
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
-
-
-def download_trade(symbol: str, date: datetime.datetime, tmp_file_path: str="./test.gz", mst_id: dict=None):
-    r = requests.get(f"{BASEURL}{MST_CONV[symbol]}/{MST_CONV[symbol]}{date.strftime('%Y-%m-%d')}.csv.gz", allow_redirects=True)
-    if r.status_code != 200: return pd.DataFrame()
+def download_trade(symbol: str, date: datetime.datetime, tmp_file_path: str="./data1.zip", mst_id: dict=None):
+    _type, _symbol = symbol.split("@")
+    url = f"{BASEURL}{MST_CONV[_type]}daily/trades/{_symbol}/{_symbol}-trades-{date.strftime('%Y-%m-%d')}.zip"
+    LOGGER.info(f"download from: {url}")
+    r   = requests.get(url, allow_redirects=True)
+    if r.status_code != 200:
+        LOGGER.warning("something is wrong. Data is missing.")
+        return pd.DataFrame()
     open(tmp_file_path, 'wb').write(r.content)
-    with gzip.open(tmp_file_path, mode="rt") as gzip_file:
-        content = gzip_file.read()
-        df = pd.read_csv(StringIO(content))
-    df["id"]       = df["trdMatchID"].astype(str)
-    df["symbol"]   = mst_id[symbol]
-    df["side"]     = df["side"].map({"Buy": 0, "Sell": 1}).astype(float).fillna(-1).astype(int)
-    df["unixtime"] = df["timestamp"].astype(int)
-    df["is_block_trade"] = False
+    with ZipFile(tmp_file_path, mode="r") as zip_file:
+        with zip_file.open(zip_file.filelist[0].filename) as f:
+            contents = f.read().decode()
+            if contents[:2] == "id":
+                df = pd.read_csv(StringIO(contents))
+            else:
+                df = pd.read_csv(StringIO(contents), header=None)
+    if df.shape[1] == 7:
+        df = df.loc[df.iloc[:, -1] == True].iloc[:, :-1]
+    df.columns = ["id", "price", "qty", "base_qty", "time", "is_buyer_maker"]
+    assert df["is_buyer_maker"].dtype == np.bool_
+    df["id"]       = df["id"].astype(np.int64)
+    df["symbol"]   = mst_id[symbol] if isinstance(mst_id, dict) and mst_id.get(symbol) is not None else symbol
+    df["side"]     = (~df["is_buyer_maker"]).astype(int) # "Buy": 0, "Sell": 1
+    df["unixtime"] = df["time"].astype(int) // 1000
+    df["size"]     = df["qty"].astype(float)
     for x in ["price", "size"]:
         df[x] = df[x].astype(float)
-    df = df.groupby(["symbol", "id"]).first().reset_index()
     return df
 
+def download_index(symbol: str, date: datetime.datetime, tmp_file_path: str="./data2.zip", mst_id: dict=None) -> list[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    _type, _symbol = symbol.split("@")
+    if _type == "SPOT": return (pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame())
+    url = f"{BASEURL}{MST_CONV[_type]}daily/metrics/{_symbol}/{_symbol}-metrics-{date.strftime('%Y-%m-%d')}.zip"
+    LOGGER.info(f"download from: {url}")
+    r   = requests.get(url, allow_redirects=True)
+    if r.status_code != 200:
+        LOGGER.warning("something is wrong. Data is missing.")
+        return (pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame())
+    open(tmp_file_path, 'wb').write(r.content)
+    with ZipFile(tmp_file_path, mode="r") as zip_file:
+        with zip_file.open(zip_file.filelist[0].filename) as f:
+            contents = f.read()
+            df = pd.read_csv(StringIO(contents.decode()))
+    df["symbol"]   = mst_id[symbol] if isinstance(mst_id, dict) and mst_id.get(symbol) is not None else symbol
+    df["unixtime"] = df["create_time"].apply(lambda x: int(datetime.datetime.strptime(x + " +0000", "%Y-%m-%d %H:%M:%S %z").timestamp()))
+    df["interval"] = 5*60
+    df["open_interest"]       = df["sum_open_interest"].astype(float)
+    df["open_interest_value"] = df["sum_open_interest_value"].astype(float)
+    df_ls_n, df_ls_ta, df_ls_tp = df.copy(), df.copy(), df.copy()
+    df_ls_n[ "ls_type"]  = LS_RATIO_TYPE["normal"]
+    df_ls_n[ "ls_ratio"] = df["count_long_short_ratio"].astype(float)
+    df_ls_n[ "long"]     = df_ls_n["ls_ratio"] / (1 + df_ls_n["ls_ratio"])
+    df_ls_n[ "short"]    = 1 - df_ls_n["long"]
+    df_ls_tp["ls_type"]  = LS_RATIO_TYPE["top_position"]
+    df_ls_tp["ls_ratio"] = df["sum_toptrader_long_short_ratio"].astype(float)
+    df_ls_tp["long"]     = df_ls_tp["ls_ratio"] / (1 + df_ls_tp["ls_ratio"])
+    df_ls_tp["short"]    = 1 - df_ls_tp["long"]
+    df_ls_ta["ls_type"]  = LS_RATIO_TYPE["top_account"]
+    df_ls_ta["ls_ratio"] = df["count_toptrader_long_short_ratio"].astype(float)
+    df_ls_ta["long"]     = df_ls_ta["ls_ratio"] / (1 + df_ls_ta["ls_ratio"])
+    df_ls_ta["short"]    = 1 - df_ls_ta["long"]
+    return (df, df_ls_n, df_ls_ta, df_ls_tp)
 
+def download_fundingrate(symbol: str, date: datetime.datetime, tmp_file_path: str="./data3.zip", mst_id: dict=None):
+    _type, _symbol = symbol.split("@")
+    if _type == "SPOT": return pd.DataFrame()
+    url = f"{BASEURL}{MST_CONV[_type]}monthly/fundingRate/{_symbol}/{_symbol}-fundingRate-{date.strftime('%Y-%m')}.zip"
+    LOGGER.info(f"download from: {url}")
+    r   = requests.get(url, allow_redirects=True)
+    if r.status_code != 200:
+        LOGGER.warning("something is wrong. Data is missing.")
+        return (pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame())
+    open(tmp_file_path, 'wb').write(r.content)
+    with ZipFile(tmp_file_path, mode="r") as zip_file:
+        with zip_file.open(zip_file.filelist[0].filename) as f:
+            contents = f.read()
+            df = pd.read_csv(StringIO(contents.decode()))
+    df["unixtime"]     = df["calc_time"] // 1000
+    df["symbol"]       = mst_id[symbol] if isinstance(mst_id, dict) and mst_id.get(symbol) is not None else symbol
+    df["funding_rate"] = df["last_funding_rate"].astype(float)
+    df["mark_price"]   = float("nan")
+    return df
+
+ 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--fr", type=lambda x: datetime.datetime.fromisoformat(str(x) + "T00:00:00Z"), help="--fr 20200101", required=True)
     parser.add_argument("--to", type=lambda x: datetime.datetime.fromisoformat(str(x) + "T00:00:00Z"), help="--to 20200101", required=True)
-    parser.add_argument("--num", type=int, default=100)
+    parser.add_argument("--num", type=int, default=1000)
     parser.add_argument("--jobs", type=int, default=1)
     parser.add_argument("--update", action='store_true', default=False)
     kwargs_psgre = {
@@ -108,27 +117,61 @@ if __name__ == "__main__":
         "keepalives_count": 100,
     }
     args   = parser.parse_args()
-    DB     = Psgre(f"host={HOST} port={PORT} dbname={DBNAME} user={USER} password={PASS}", kwargs_psgre=kwargs_psgre, max_disp_len=200)
-    df_mst = DB.select_sql(f"select * from master_symbol where is_active = true and exchange = '{EXCHANGE}'")
+    res    = requests.post("http://127.0.0.1:8000/select", json={"sql": f"select * from master_symbol where is_active = true and exchange = '{EXCHANGE}'"}, headers={'Content-type': 'application/json'})
+    df_mst = pd.DataFrame(json.loads(res.json()))
     mst_id = {y:x for x, y in df_mst[["symbol_id", "symbol_name"]].values}
     date_st, date_ed = args.fr, args.to
     assert date_st <= date_ed
     for date in [date_st + datetime.timedelta(days=x) for x in range((date_ed - date_st).days + 1)]:
         for symbol in mst_id.keys():
-            DB.logger.info(f"date: {date}, symbol: {symbol}")
-            if MST_CONV.get(symbol) is None: continue
+            LOGGER.info(f"date: {date}, symbol: {symbol}")
+            # executions
             df = download_trade(symbol, date, mst_id=mst_id)
             if df.shape[0] > 0:
-                df_exist = DB.select_sql(f"select id from {EXCHANGE}_executions where symbol = {df['symbol'].iloc[0]} and unixtime >= {int(df['unixtime'].min())} and unixtime <= {int(df['unixtime'].max())};")
-                df = df.loc[~df["id"].isin(df_exist["id"])]
+                res      = requests.post("http://127.0.0.1:8000/select", json={"sql": f"select id from {EXCHANGE}_executions where symbol = {df['symbol'].iloc[0]} and unixtime >= {int(df['unixtime'].min())} and unixtime <= {int(df['unixtime'].max())};"}, headers={'Content-type': 'application/json'})
+                df_exist = pd.DataFrame(json.loads(res.json()))
+                df       = df.loc[~df["id"].isin(df_exist["id"])]
             else:
-                DB.logger.warning("Nothing data.")
+                LOGGER.warning("Nothing data.")
                 continue
             if df.shape[0] > 0 and args.update:
                 if df.shape[0] >= args.num:
                     for indexes in tqdm(np.array_split(np.arange(df.shape[0]), df.shape[0] // args.num)):
-                        DB.insert_from_df(df.iloc[indexes], f"{EXCHANGE}_executions", is_select=True, n_jobs=args.jobs)
-                        DB.execute_sql()
+                        res = requests.post("http://127.0.0.1:8000/insert", json={"data": df.iloc[indexes].replace({float("nan"): None}).to_dict(), "tblname": f"{EXCHANGE}_executions", "is_select": True}, headers={'Content-type': 'application/json'})
+                        assert res.status_code == 200
                 else:
-                    DB.insert_from_df(df, f"{EXCHANGE}_executions", is_select=True, n_jobs=args.jobs)
-                    DB.execute_sql()
+                    res = requests.post("http://127.0.0.1:8000/insert", json={"data": df.replace({float("nan"): None}).to_dict(), "tblname": f"{EXCHANGE}_executions", "is_select": True}, headers={'Content-type': 'application/json'})
+                    assert res.status_code == 200
+            # other index
+            df_oi, df_ls_n, df_ls_ta, df_ls_tp = download_index(symbol, date, mst_id=mst_id)
+            df = df_oi.copy()
+            if df.shape[0] > 0 and args.update:
+                res = requests.post("http://127.0.0.1:8000/insert", json={
+                    "data": df.replace({float("nan"): None}).to_dict(), "tblname": f"{EXCHANGE}_open_interest", "is_select": True,
+                    "add_sql": (
+                        f"delete from {EXCHANGE}_open_interest where symbol = {df['symbol'].iloc[0]} and " + 
+                        f"unixtime >= {int(df['unixtime'].min())} and unixtime <= {int(df['unixtime'].max())};"
+                    ) # latest data is more accurete. The data within 60s wouldn't be completed.
+                }, headers={'Content-type': 'application/json'})
+                assert res.status_code == 200
+            for df in [df_ls_n, df_ls_ta, df_ls_tp]:
+                if df.shape[0] > 0 and args.update:
+                    res = requests.post("http://127.0.0.1:8000/insert", json={
+                        "data": df.replace({float("nan"): None}).to_dict(), "tblname": f"{EXCHANGE}_long_short", "is_select": True,
+                        "add_sql": (
+                            f"delete from {EXCHANGE}_long_short where symbol = {df['symbol'].iloc[0]} and ls_type = {df['ls_type'].iloc[0]} and " + 
+                            f"unixtime >= {int(df['unixtime'].min())} and unixtime <= {int(df['unixtime'].max())};"
+                        ) # latest data is more accurete. The data within 60s wouldn't be completed.
+                    }, headers={'Content-type': 'application/json'})
+                    assert res.status_code == 200
+            # funding rate
+            df = download_fundingrate(symbol, date, mst_id=mst_id)
+            if df.shape[0] > 0 and args.update:
+                res = requests.post("http://127.0.0.1:8000/insert", json={
+                    "data": df.replace({float("nan"): None}).to_dict(), "tblname": f"{EXCHANGE}_funding_rate", "is_select": True,
+                    "add_sql": (
+                        f"delete from {EXCHANGE}_funding_rate where symbol = {df['symbol'].iloc[0]} and " + 
+                        f"unixtime >= {int(df['unixtime'].min())} and unixtime <= {int(df['unixtime'].max())};"
+                    ) # latest data is more accurete. The data within 60s wouldn't be completed.
+                }, headers={'Content-type': 'application/json'})
+                assert res.status_code == 200
