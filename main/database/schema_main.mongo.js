e5459913d4fdd6e29fed4b2a381703048e966fec
kazukingh01 <kazukingh01@gmail.com>
2024-09-25 20:05:47 +0900

    modify

---
+++ main/database/schema_main.mongo.js
@@ -0,0 +1,5 @@
+use trade;
+
+db.getSiblingDB("trade").getCollection("master_symbol").drop();
+db.getSiblingDB("trade").getCollection("master_symbol").createIndex({ symbol_id: 1 }, { unique: true });
+db.getSiblingDB("trade").getCollection("master_symbol").createIndex({ exchange: 1, symbol_name: 1 }, { unique: true });
