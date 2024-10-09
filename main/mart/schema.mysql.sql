4687812569aa6860f48ce0891341d264bca8bc34
kazukingh01 <kazukingh01@gmail.com>
2024-10-09 23:37:58 +0900

    modify

--- main/mart/schema.mysql.sql
+++ main/mart/schema.mysql.sql
@@ -1,6 +1,7 @@
 CREATE TABLE mart_ohlc (
     symbol SMALLINT NOT NULL,
     unixtime DATETIME NOT NULL,
+    type SMALLINT NOT NULL,
     `interval` INT NOT NULL,
     open FLOAT,
     high FLOAT,
@@ -8,9 +9,10 @@ CREATE TABLE mart_ohlc (
     close FLOAT,
     ave FLOAT,
     attrs JSON,
-    PRIMARY KEY (symbol, unixtime, `interval`)
+    PRIMARY KEY (symbol, unixtime, type, `interval`)
 );
 
 CREATE INDEX mart_ohlc_0 ON mart_ohlc (symbol);
 CREATE INDEX mart_ohlc_1 ON mart_ohlc (unixtime);
-CREATE INDEX mart_ohlc_2 ON mart_ohlc (`interval`);
+CREATE INDEX mart_ohlc_2 ON mart_ohlc (type);
+CREATE INDEX mart_ohlc_3 ON mart_ohlc (`interval`);
--- main/mart/schema.psgre.sql
+++ main/mart/schema.psgre.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.mart_ohlc (
     symbol smallint NOT NULL,
     unixtime timestamp with time zone NOT NULL,
+    type smallint NOT NULL,
     "interval" integer NOT NULL,
     open real,
     high real,
@@ -11,8 +12,9 @@ CREATE TABLE public.mart_ohlc (
 );
 
 ALTER TABLE ONLY public.mart_ohlc
-    ADD CONSTRAINT mart_ohlc_pkey PRIMARY KEY (symbol, unixtime, "interval");
+    ADD CONSTRAINT mart_ohlc_pkey PRIMARY KEY (symbol, unixtime, type, "interval");
 
 CREATE INDEX mart_ohlc_0 ON public.mart_ohlc USING btree (symbol);
 CREATE INDEX mart_ohlc_1 ON public.mart_ohlc USING btree (unixtime);
-CREATE INDEX mart_ohlc_2 ON public.mart_ohlc USING btree ("interval");
+CREATE INDEX mart_ohlc_2 ON public.mart_ohlc USING btree (type);
+CREATE INDEX mart_ohlc_3 ON public.mart_ohlc USING btree ("interval");
