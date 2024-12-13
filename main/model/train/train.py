64a955e49de5450e9e327a10ef692aeabfd2c534
kazukingh01 <kazukingh01@gmail.com>
2024-12-13 10:00:10 +0900

    modify

--- main/model/train/train.py
+++ main/model/train/train.py
@@ -2,8 +2,8 @@ import glob, argparse
 import pandas as pd
 import numpy as np
 # local package
-from kkgbdt.model import KkGBDT
-from kkmlmanager.manager import MLManager
+# from kkgbdt.model import KkGBDT
+from kkmlmanager.manager import MLManager, load_manager
 from kklogger import set_logger
 from scipy.stats import norm
 
@@ -17,12 +17,20 @@ assert BASE_SR == BASE_ITVLS[0]
 
 if __name__ == "__main__":
     parser  = argparse.ArgumentParser()
-    parser.add_argument("--load", type=str)
+    parser.add_argument("--dir",     type=str)
+    parser.add_argument("--dfsave",  type=str)
+    parser.add_argument("--dfload",  type=str)
+    parser.add_argument("--manager", type=str)
     parser.add_argument("--save", action='store_true', default=False)
     args = parser.parse_args()
     LOGGER.info(f"args: {args}")
-    if args.load is None:
-        list_data = glob.glob("./data/*.pickle")
+    if args.dfload is None:
+        assert args.dir is not None
+    else:
+        assert args.dir    is None
+        assert args.dfsave is None
+    if args.dfload is None:
+        list_data = glob.glob(f"{args.dr}/*.pickle")
         df = pd.concat([pd.read_pickle(x) for x in list_data], axis=0, ignore_index=False, sort=False)
         ndf_sbls  = np.unique(df.columns[np.where(df.columns == "===")[0][0] + 1:].str.split("_").str[-1])
         ndf_itbls = np.unique(df.columns[np.where(df.columns == "===")[0][0] + 1:].str.split("_").str[-2]).astype(int)
@@ -30,27 +38,31 @@ if __name__ == "__main__":
             for sbl in ndf_sbls:
                 df[f"gt@ave_in{  int(itvl + BASE_SR)}_{itvl}_{sbl}"] = df[f"gt@ave_{itvl}_{sbl}"].shift(-int((itvl + BASE_SR) // BASE_SR)) # predict in 2+2min, 8+2min and 40+2min. Additional 2min is prepared for processing of creating mart data.
                 df[f"gt@ratio_in{int(itvl + BASE_SR)}_{itvl}_{sbl}"] = df[f"gt@ave_in{int(itvl + BASE_SR)}_{itvl}_{sbl}"] / df[f"gt@close_{BASE_SR}_{sbl}"]
-        if args.save:
-            df.to_pickle("df_train.pickle")
+        # create answer
+        columns_ans = [f"gt@ratio_in{int(itvl + BASE_SR)}_{itvl}_s{sbl}" for itvl in BASE_ITVLS for sbl in SYMBOLS_ANS]
+        ndf_val     = df.loc[:, columns_ans].values.copy()
+        ndf_ans     = np.ones(df.loc[:, columns_ans].shape) * -1
+        ndf_mean    = df[columns_ans].mean().values.reshape(1, -1)
+        ndf_sigma   = df[columns_ans].std(). values.reshape(1, -1)
+        z_values    = [-float("inf")] + [norm.ppf(p) for p in [0.1, 0.3, 0.7, 0.9]] + [float("inf")]
+        for i, x in enumerate(z_values[:-1]):
+            ndf_bool = (ndf_val >= (ndf_mean + (x * ndf_sigma))) & (ndf_val < (ndf_mean + (z_values[i+1] * ndf_sigma)))
+            ndf_ans[ndf_bool] = i
+        df = pd.concat([df, pd.DataFrame(ndf_ans.astype(int), index=df.index, columns=pd.Index(columns_ans).str.replace("gt@ratio", "gt@cls"))], axis=1, ignore_index=False)
+        if args.dfsave is not None:
+            df.to_pickle(f"{args.dfsave}")
     else:
-        df = pd.read_pickle(args.load)
-    # create answer
-    columns_ans = [f"gt@ratio_in{int(itvl + BASE_SR)}_{itvl}_s{sbl}" for itvl in BASE_ITVLS for sbl in SYMBOLS_ANS]
-    ndf_val     = df.loc[:, columns_ans].values.copy()
-    ndf_ans     = np.ones(df.loc[:, columns_ans].shape) * -1
-    ndf_mean    = df[columns_ans].mean().values.reshape(1, -1)
-    ndf_sigma   = df[columns_ans].std(). values.reshape(1, -1)
-    z_values    = [-float("inf")] + [norm.ppf(p) for p in [0.1, 0.3, 0.7, 0.9]] + [float("inf")]
-    for i, x in enumerate(z_values[:-1]):
-        ndf_bool = (ndf_val >= (ndf_mean + (x * ndf_sigma))) & (ndf_val < (ndf_mean + (z_values[i+1] * ndf_sigma)))
-        ndf_ans[ndf_bool] = i
-    df = pd.concat([df, pd.DataFrame(ndf_ans.astype(int), index=df.index, columns=pd.Index(columns_ans).str.replace("gt@ratio", "gt@cls"))], axis=1, ignore_index=False)
+        df = pd.read_pickle(args.dfload)
     # preprocess
-    manager = MLManager(df.columns[:np.where(df.columns == "===")[0][0]].tolist(), "gt@cls_in240_120_s14", n_jobs=8)
-    manager.cut_features_by_variance(df, cutoff=0.9, ignore_nan=False, batch_size=16)
-    manager.cut_features_by_randomtree_importance(df, cutoff=None, max_iter=10, min_count=100)
-    manager.save("./mlmanager")
-    manager.cut_features_by_correlation(df, cutoff=0.95, dtype='float32', is_gpu=False, corr_type='pearson', batch_size=1, min_n=100)
+    if args.manager is None:
+        manager = MLManager(df.columns[:np.where(df.columns == "===")[0][0]].tolist(), "gt@cls_in240_120_s14", n_jobs=8)
+        manager.cut_features_by_variance(df, cutoff=0.9, ignore_nan=False, batch_size=16)
+        manager.cut_features_by_randomtree_importance(df, cutoff=None, max_iter=10, min_count=100)
+        if args.save:
+            manager.save("./mlmanager", exist_ok=True)
+        manager.cut_features_by_correlation(df, cutoff=0.95, dtype='float32', is_gpu=False, corr_type='pearson', batch_size=1, min_n=100)
+    else:
+        manager = load_manager(args.manager, 8)
     # set model
     # manager.set_model(
     #     KkGBDT, 5, model_func_predict="predict",
