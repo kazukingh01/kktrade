3af57aaf735813017451f41f59bf4a6171941f9d
kazukingh01 <kazukingh01@gmail.com>
2024-12-20 14:25:18 +0900

    modify

--- main/model/eval/eval.py
+++ main/model/eval/eval.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, datetime
 import pandas as pd
 import numpy as np
 from kkmlmanager.manager import load_manager
@@ -19,19 +19,25 @@ if __name__ == "__main__":
     parser  = argparse.ArgumentParser()
     parser.add_argument("--dfload", type=str)
     parser.add_argument("--mlload", type=str)
+    parser.add_argument("--dfsave", type=str)
     parser.add_argument("--njob",   type=int, default=1)
     args = parser.parse_args()
     LOGGER.info(f"args: {args}")
-    df = pd.read_pickle(args.dfload)
-    manager = load_manager(args.mlload, args.njob)
-    output, input_y, input_index = manager.predict(df=df)
-    COL_ANS             = manager.columns_ans[0].replace("cls_", "ave_")
-    colname_base_price  = COL_ANS.split("_")[0] + "_120_" + COL_ANS.split("_")[-1]
-    colname_entry_price = (COL_ANS.split("_")[0] + "_in120_120_" + COL_ANS.split("_")[-1]).replace("ave_", "close_")
-    colname_close_price = COL_ANS.replace("ave_", "close_")
-    colname_low_price   = COL_ANS.replace("ave_", "low_")
-    colname_high_price  = COL_ANS.replace("ave_", "high_")
-    df_pred = pd.DataFrame(output, columns=[f"pred_{x}" for x in range(output.shape[-1])], index=input_index)
+    assert args.dfload is not None
+    if args.mlload is not None:
+        df = pd.read_pickle(args.dfload)
+        manager = load_manager(args.mlload, args.njob)
+        output, input_y, input_index = manager.predict(df=df)
+        COL_ANS             = manager.columns_ans[0].replace("cls_", "ave_")
+        colname_base_price  = COL_ANS.split("_")[0] + "_120_" + COL_ANS.split("_")[-1]
+        colname_entry_price = (COL_ANS.split("_")[0] + "_in120_120_" + COL_ANS.split("_")[-1]).replace("ave_", "close_")
+        df_pred = pd.DataFrame(output, columns=[f"pred_{x}" for x in range(output.shape[-1])], index=input_index)
+        df_pred[[colname_base_price, colname_entry_price]] = df[[colname_base_price, colname_entry_price]].copy()
+        if args.dfsave is not None:
+            df_pred.to_pickle(f"{args.dfsave}")
+    else:
+        df_pred = pd.read_pickle(args.dfload)
+        colname_base_price, colname_entry_price = df_pred.columns[-2:]
     df_pred["pred_sell"] = df_pred[[f"pred_{x}" for x in CLS_SELL]].sum(axis=1)
     df_pred["pred_buy" ] = df_pred[[f"pred_{x}" for x in CLS_BUY ]].sum(axis=1)
     df_pred["is_cond_pred_sell"] = (df_pred["pred_sell"] >= THRE_SELL)
@@ -39,10 +45,10 @@ if __name__ == "__main__":
     boolwk = (df_pred["is_cond_pred_sell"] & df_pred["is_cond_pred_buy"]) & (df_pred["pred_sell"] > df_pred["pred_buy"])
     df_pred.loc[boolwk, "is_cond_pred_sell"] = True
     df_pred.loc[boolwk, "is_cond_pred_buy" ] = False
-    df_pred[[colname_base_price, colname_entry_price]] = df[[colname_base_price, colname_entry_price]].copy()
     list_entry, status, list_fees, list_return = [], None, [], []
     for i_entry, (price_base, price_entry, is_cond_pred_sell, is_cond_pred_buy) in enumerate(df_pred[[colname_base_price, colname_entry_price, "is_cond_pred_sell", "is_cond_pred_buy"]].values):
         is_sell, is_buy = False, False
+        strdate = datetime.datetime.fromtimestamp(df_pred.index[2], tz=datetime.UTC).strftime("%Y-%m-%d %H:%M")
         if is_cond_pred_sell:
             if (price_base * RATIO_SELL) < price_entry:
                 is_sell = True
@@ -52,17 +58,17 @@ if __name__ == "__main__":
         if is_sell:
             if status is None:
                 assert len(list_entry) == 0
-                LOGGER.info(f"i: {i_entry}, SELL     !!!!!!")
+                LOGGER.info(f"{strdate}, SELL     !!!!!!")
                 list_entry.append(price_entry)
                 list_fees. append(FEE_TAKER)
                 status = "sell"
             elif status == "sell":
-                LOGGER.info(f"i: {i_entry}, CONTINUE !!!!!!")
+                LOGGER.info(f"{strdate}, CONTINUE !!!!!!")
                 list_entry.append(price_entry)
                 list_fees. append(FEE_TAKER)
             elif status == "buy":
                 amount_ret = ((np.array(list_entry) - price_entry) / price_entry).sum()
-                LOGGER.info(f"i: {i_entry}, status: {status}, retrun: {amount_ret}")
+                LOGGER.info(f"{strdate}, status: {status}, retrun: {amount_ret}")
                 list_return.append(amount_ret)
                 list_fees. append(FEE_TAKER)
                 status = None
@@ -70,19 +76,19 @@ if __name__ == "__main__":
         elif is_buy:
             if status is None:
                 assert len(list_entry) == 0
-                LOGGER.info(f"i: {i_entry}, BUY      !!!!!!")
+                LOGGER.info(f"{strdate}, BUY      !!!!!!")
                 list_entry.append(price_entry)
                 list_fees. append(FEE_TAKER)
                 status = "buy"
             elif status == "sell":
                 amount_ret = (-1 * (np.array(list_entry) - price_entry) / price_entry).sum()
-                LOGGER.info(f"i: {i_entry}, status: {status}, retrun: {amount_ret}")
+                LOGGER.info(f"{strdate}, status: {status}, retrun: {amount_ret}")
                 list_return.append(amount_ret)
                 list_fees. append(FEE_TAKER)
                 status = None
                 list_entry = []
             elif status == "buy":
-                LOGGER.info(f"i: {i_entry}, CONTINUE !!!!!!")
+                LOGGER.info(f"{strdate}, CONTINUE !!!!!!")
                 list_entry.append(price_entry)
                 list_fees. append(FEE_TAKER)
         if (i_entry == (df_pred.shape[0] - 1)) and (len(list_entry) > 0):
