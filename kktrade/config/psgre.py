adcd19ba16782e741fcb7b7db1ae79ef66082afc
kazukingh01 <kazukingh01@gmail.com>
2025-01-13 22:47:12 +0900

    modify

--- kktrade/config/psgre.py
+++ kktrade/config/psgre.py
@@ -7,9 +7,23 @@ __all__ = [
     "DBTYPE",
 ]
 
-HOST   = "127.0.0.1"
-PORT   = 5432
-USER   = "postgres"
-PASS   = "postgres"
-DBNAME = "trade"
-DBTYPE = "psgre"
+HOST_BS   = "127.0.0.1"
+PORT_BS   = 5432
+USER_BS   = "postgres"
+PASS_BS   = "postgres"
+DBNAME_BS = "trade"
+DBTYPE_BS = "psgre"
+
+HOST_BK   = "127.0.0.2"
+PORT_BK   = 22017
+USER_BK   = "admin"
+PASS_BK   = "admin"
+DBNAME_BK = "trade"
+DBTYPE_BK = "mongo"
+
+HOST   = HOST_BS  
+PORT   = PORT_BS  
+USER   = USER_BS  
+PASS   = PASS_BS  
+DBNAME = DBNAME_BS
+DBTYPE = DBTYPE_BS
